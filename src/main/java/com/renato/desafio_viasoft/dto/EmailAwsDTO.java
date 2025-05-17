6c844e71d6ea2c5a1b4462f3c9b961e0e3f5b485
Renato Muller Reinhold <renatoreinhold2@gmail.com>
2025-05-17 19:19:53 -0300

    DTOs

--- src/main/java/com/renato/desafio_viasoft/dto/EmailAwsDTO.java
+++ src/main/java/com/renato/desafio_viasoft/dto/EmailAwsDTO.java
@@ -1,4 +1,13 @@
 package com.renato.desafio_viasoft.dto;
 
+import lombok.Data;
+
+@Data
 public class EmailAwsDTO {
+    private String recipient;
+    private String recipientName;
+    private String sender;
+    private String subject;
+    private String content;
+    
 }
--- src/main/java/com/renato/desafio_viasoft/dto/EmailOciDTO.java
+++ src/main/java/com/renato/desafio_viasoft/dto/EmailOciDTO.java
@@ -1,4 +1,13 @@
 package com.renato.desafio_viasoft.dto;
 
+import lombok.Data;
+
+@Data
 public class EmailOciDTO {
+    private String recipientEmail;
+    private String recipientName;
+    private String senderEmail;
+    private String subject;
+    private String body;
+
 }
